6edf933765f0a1cb6117fbfb2f0fcb3785b646b3
Edson00376920 <00376920@uca.edu.sv>
2026-06-15 23:25:29 -0600

    feat: agregar repositorios de Role y User

---
+++ Telemedicina/src/main/java/com/example/telemedicina/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.telemedicina.repositories;
+
+
+import com.example.telemedicina.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // Para autenticación o validación de duplicados
+    Optional<User> findByEmail(String email);
+
+    // Para buscar solo usuarios que no hayan sufrido borrado lógico
+    Long countByEmailAndIsActiveTrue(String email);
+}
