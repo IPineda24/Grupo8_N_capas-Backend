008e4e0dc8f09d2133d96942fc00c597165bb151
SamMontes <sammontezz.sm@gmail.com>
2026-06-16 22:25:07 -0600

    chore: Añadiendo .env.example y corrigiendo nombre de rol en contrato API

--- Telemedicina/src/main/java/com/example/telemedicina/TelemedicinaApplication.java
+++ Telemedicina/src/main/java/com/example/telemedicina/TelemedicinaApplication.java
@@ -1,7 +1,10 @@
 package com.example.telemedicina;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class TelemedicinaApplication {
