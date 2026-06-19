b04d410731733f2fd26df10ed3fad06350c00688
Edson00376920 <00376920@uca.edu.sv>
2026-06-19 00:58:11 -0600

    feature: crear JwtUtil y agregar su logica

---
+++ Telemedicina/src/main/java/com/example/telemedicina/security/JwtUtil.java
@@ -0,0 +1,62 @@
+package com.example.telemedicina.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    private final long TOKEN_VALIDITY = 1000 * 60 * 60 * 24;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities());
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .claims(claims) // En 0.12 se usa .claims() en lugar de .setClaims()
+                .subject(subject) // .subject() en lugar de .setSubject()
+                .issuedAt(new Date(System.currentTimeMillis())) // .issuedAt() en lugar de .setIssuedAt()
+                .expiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY)) // .expiration() en lugar de .setExpiration()
+                .signWith(SECRET_KEY) // Ahora acepta el SecretKey directamente sin especificar el algoritmo
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+}
