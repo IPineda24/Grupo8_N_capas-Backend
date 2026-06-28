fc2529ba532b781455b5d019ef7758559c8375ed
Irvin Pineda <00024420@uca.edu.sv>
2026-06-28 10:14:07 -0600

    Add swagger

--- src/main/java/com/uca/telemedicina/configuration/SecurityConfiguration.java
+++ src/main/java/com/uca/telemedicina/configuration/SecurityConfiguration.java
@@ -1,4 +1,5 @@
 package com.uca.telemedicina.configuration;
+
 import com.uca.telemedicina.security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.*;
@@ -13,28 +14,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration @EnableWebSecurity @EnableMethodSecurity @RequiredArgsConstructor
 public class SecurityConfiguration {
     private final JwtAuth jwtAuth;
     private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/specialties/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/doctors/**").permitAll()
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated())
-            .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/specialties/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/doctors/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
         http.exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuth));
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration cfg) throws Exception {
         return cfg.getAuthenticationManager();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 }
